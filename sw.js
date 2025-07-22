bb5dc51a78b12501883fb80c112aa2f065547131
Harshit Mogha <moghaeashu@gmail.com>
2025-07-22 11:53:47 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+}); 
