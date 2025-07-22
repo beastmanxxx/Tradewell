fa4c72452702f155bdb8c4836ca9d526c0cd341b
Harshit Mogha <moghaeashu@gmail.com>
2025-07-22 16:06:38 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,4 +10,11 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data && event.notification.data.url ? event.notification.data.url : 'https://tradewell.kesug.com/')
+  );
 }); 
